978eaa7de0195fd442016d1afe027b499255b1c7
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2022-12-08 16:40:39 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,50 +1,50 @@
 <?php
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8';
-$mail->setLanguage('ru', 'phpmailer/language/');
-$mail->IsHTML(true);
-$mail->setFrom('comandatt@yandex.ru', 'Евгений');
-$mail->addAddress('comandatt@yandex.ru');
-$mail->Subject = 'Привет!';
-$hand = "Правая";
-if($_POST['hand'] == "left") {
-$hand = "Левая";
-}
-$body = '<h1>Встречайте письмо!</h1>';
-if(trim(!empty($_POST['name']))) {
-$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
-}
-if(trim(!empty($_POST['email']))) {
-$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
-}
-if(trim(!empty($_POST['hand']))) {
-$body.='<p><strong>Рука:</strong> '.$hand['hand'].'</p>';
-}
-if(trim(!empty($_POST['age']))) {
-$body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
-}
-if(trim(!empty($_POST['message']))) {
-$body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
-}
-if(!empty($_FILES['image']['tmp_name'])) {
-$filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
-if (copy($_FILES['image']['tmp_name'], $filePath)) {
-$fileAttach = $filePath;
-$body.='<p><strong>Фото в приложении</strong>';
-$mail->addAttachment($fileAttach);
+  require 'phpmailer/src/Exception.php';
+  require 'phpmailer/src/PHPMailer.php';
+  $mail = new PHPMailer(true);
+  $mail->CharSet = 'UTF-8';
+  $mail->setLanguage('ru', 'phpmailer/language/');
+  $mail->IsHTML(true);
+  $mail->setFrom('comandatt@yandex.ru', 'Евгений');
+  $mail->addAddress('comandatt@yandex.ru');
+  $mail->Subject = 'Привет!';
+  $hand = "Правая";
+  if($_POST['hand'] == "left") {
+     $hand = "Левая";
   }
-}
-$mail->Body = $body;
-if (!mail->send()) {
-$message = 'Ошибка';
-} else {
-$message = 'Данные отправлены';
-}
-$response = ['message' => $message];
-header('Content-type: application/json');
-echo json_encode($response);
+  $body = '<h1>Встречайте письмо!</h1>';
+  if(trim(!empty($_POST['name']))) {
+    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+  }
+  if(trim(!empty($_POST['email']))) {
+    $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+  }
+  if(trim(!empty($_POST['hand']))) {
+    $body.='<p><strong>Рука:</strong> '.$hand['hand'].'</p>';
+  }
+  if(trim(!empty($_POST['age']))) {
+    $body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
+  }
+  if(trim(!empty($_POST['message']))) {
+    $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+  }
+  if (!empty($_FILES['image']['tmp_name'])) {
+    $filePath = __DIR__ . "/files/" . $_FILES['image']['name'];
+    if (copy($_FILES['image']['tmp_name'], $filePath)) {
+      $fileAttach = $filePath;
+      $body.='<p><strong>Фото в приложении</strong>';
+      $mail->addAttachment($fileAttach);
+    }
+  }
+  $mail->Body = $body;
+  if (!$mail->send()) {
+    $message = 'Ошибка';
+  } else {
+    $message = 'Данные отправлены';
+  }
+  $response = ['message' => $message];
+  header('Content-type: application/json');
+  echo json_encode($response);
 ?>
