d8bb263dbf27de791bfd3bed24f3b0b675aab112
EvgeniyR1987 <103533983+EvgeniyR1987@users.noreply.github.com>
2022-12-10 22:11:17 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,15 +1,15 @@
 <?php
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
-  require 'phpmailer/src/Exception.php';
-  require 'phpmailer/src/PHPMailer.php';
+  require 'phpmailer/Exception.php';
+  require 'phpmailer/PHPMailer.php';
   $mail = new PHPMailer(true);
   $mail->CharSet = 'UTF-8';
   $mail->setLanguage('ru', 'phpmailer/language/');
   $mail->IsHTML(true);
   $mail->setFrom('comandatt@yandex.ru', 'Евгений');
   $mail->addAddress('comandatt@yandex.ru');
-  $mail->Subject = 'Привет!';
+  $mail->Subject = 'Форма';
   $hand = "Правая";
   if($_POST['hand'] == "left") {
      $hand = "Левая";
@@ -22,7 +22,7 @@
     $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
   }
   if(trim(!empty($_POST['hand']))) {
-    $body.='<p><strong>Рука:</strong> '.$hand['hand'].'</p>';
+    $body.='<p><strong>Рука:</strong> '.$hand.'</p>';
   }
   if(trim(!empty($_POST['age']))) {
     $body.='<p><strong>Возраст:</strong> '.$_POST['age'].'</p>';
